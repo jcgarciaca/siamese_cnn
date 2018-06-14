a4d3696426a86109e210d53942204498bdc23906
jcgarciaca <julio_jcgc@hotmail.com>
2018-06-14 10:00:25 -0500

    Update siamese_cnn_vgg16.py

--- siamese_cnn_vgg16.py
+++ siamese_cnn_vgg16.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 import random
 from keras.datasets import mnist
@@ -23,11 +20,7 @@ from keras.applications import VGG16
 from skimage.transform import rescale, resize
 import skimage.io as io
 
-
 np.random.seed(1337)  # for reproducibility
-
-
-# In[2]:
 
 
 # convolutional base
@@ -52,9 +45,6 @@ for layer in conv_base.layers:
         layer.trainable = False
         
 conv_base.summary()
-
-
-# In[3]:
 
 
 def euclidean_distance(vects):
@@ -86,22 +76,12 @@ def create_data_siamese_from_images(path1, path2, data_size):
     
     # true pairs
     images_in_path1 = os.listdir(path1)    
-    images_in_path2 = os.listdir(path2)
-        
-    # print("len(images_in_path1): ", len(images_in_path1))
+    images_in_path2 = os.listdir(path2)    
     
-    # np.random.shuffle(images_in_path1)
-    
-    # print(np.random.randint(0, len(images_in_path1) - 1))
-    
-    
-    for i in range(data_size):
-        
+    for i in range(data_size):        
         # true data C1
         index1 = np.random.randint(0, len(images_in_path1) - 1)
-        index2 = np.random.randint(0, len(images_in_path1) - 1)        
-        # print("image1: ", images_in_path1[index1])
-        # print("image2: ", images_in_path1[index2])
+        index2 = np.random.randint(0, len(images_in_path1) - 1)
         img1 = io.imread(os.path.join(path1, images_in_path1[index1]))
         img2 = io.imread(os.path.join(path1, images_in_path1[index2]))                
         img1 = resize(img1, (WIDTH, HEIGHT), anti_aliasing = True)
@@ -113,9 +93,7 @@ def create_data_siamese_from_images(path1, path2, data_size):
         
         # true data C2
         index1 = np.random.randint(0, len(images_in_path2) - 1)
-        index2 = np.random.randint(0, len(images_in_path2) - 1)        
-        # print("image1: ", images_in_path2[index1])
-        # print("image2: ", images_in_path2[index2])
+        index2 = np.random.randint(0, len(images_in_path2) - 1)
         img1 = io.imread(os.path.join(path2, images_in_path2[index1]))
         img2 = io.imread(os.path.join(path2, images_in_path2[index2]))                
         img1 = resize(img1, (WIDTH, HEIGHT), anti_aliasing = True)
@@ -128,9 +106,7 @@ def create_data_siamese_from_images(path1, path2, data_size):
         
         # false data 1
         index1 = np.random.randint(0, len(images_in_path1) - 1)
-        index2 = np.random.randint(0, len(images_in_path2) - 1)        
-        # print("image1: ", images_in_path1[index1])
-        # print("image2: ", images_in_path2[index2])
+        index2 = np.random.randint(0, len(images_in_path2) - 1)
         
         if(i%2 == 0):
             img1 = io.imread(os.path.join(path1, images_in_path1[index1]))
@@ -148,9 +124,7 @@ def create_data_siamese_from_images(path1, path2, data_size):
         
         # false data 2
         index1 = np.random.randint(0, len(images_in_path1) - 1)
-        index2 = np.random.randint(0, len(images_in_path2) - 1)        
-        # print("image1: ", images_in_path1[index1])
-        # print("image2: ", images_in_path2[index2])
+        index2 = np.random.randint(0, len(images_in_path2) - 1)
         
         if(i%2 == 0):
             img1 = io.imread(os.path.join(path1, images_in_path1[index1]))
@@ -169,51 +143,6 @@ def create_data_siamese_from_images(path1, path2, data_size):
     return(np.array(set1), np.array(set2), np.array(labels))
 
 
-def create_data_siamese(x, digit_indices):
-    set1 = []
-    set2 = []
-    labels = []
-    # print(len(digit_indices[0]))
-    # print(len(digit_indices[1]))
-    n = min([len(digit_indices[d]) for d in range(2)])
-    if(n > 50):
-        n = 50
-    
-    print("min: ", n)
-    
-    # true pairs
-    for i in range(n):
-        for j in range(n):
-            if(i != j):
-                z1, z2 = digit_indices[0][i], digit_indices[0][j]
-                set1.append(x[z1])
-                set2.append(x[z2])
-                labels.append(1)
-                z1, z2 = digit_indices[1][i], digit_indices[1][j]
-                set1.append(x[z1])
-                set2.append(x[z2])
-                labels.append(1)
-    
-    print("set1: ", len(set1))
-    print("set2: ", len(set2))
-    
-    # false pairs
-    for i in range(n):
-        for j in range(2 * n):
-            if(i != j):
-                z1, z2 = digit_indices[0][i], digit_indices[1][j]
-                set1.append(x[z1])
-                set2.append(x[z2])
-                labels.append(0)                
-    
-    print("set1: ", len(set1))
-    print("set2: ", len(set2))
-    
-    return(np.array(set1), np.array(set2), np.array(labels))
-    
-    
-    
-          
 def create_pairs(x, digit_indices):
     '''Positive and negative pair creation.
     Alternates between positive and negative pairs.
@@ -241,44 +170,7 @@ def compute_accuracy(predictions, labels):
     return np.mean(labels == (predictions.ravel() > 0.5))
 
 
-# In[4]:
-
-
-'''
-# the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-# X_train = X_train.reshape(60000, 784)
-# X_test = X_test.reshape(10000, 784)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-
-# input_dim = 784
-
-nb_epochs = 20
-
-print("----------------------------")
-print(X_train.shape)
-print(y_train.shape)
-
-print(X_test.shape)
-print(y_test.shape)
-print("----------------------------")
-'''
-'''
-for index in range(5):
-    plt.figure(index)
-    imgplot = plt.imshow(X_train[index])
-    print(y_train[index])
-'''
-
-
-# In[5]:
-
-
-# the data split between train and test sets
+# data split between train and test sets
 original_dataset_dir = '/home/jcgarcia_mill/cnn_test/cats_dogs/train' #'/home/JulioCesar/cnn_test/cats_dogs/train'
 base_dir = '/home/jcgarcia_mill/cnn_test/cats_dogs/cats_and_dogs_small' #'/home/JulioCesar/cnn_test/cats_dogs/cats_and_dogs_small'
 
@@ -298,66 +190,17 @@ test_dogs_dir = os.path.join(test_dir, 'dogs')
 
 data_size = 2000
 
+# generate train data (true and false)
 tr_set1, tr_set2, tr_y = create_data_siamese_from_images(train_cats_dir, train_dogs_dir, int(data_size / 4))
 
 print(tr_set1.shape)
 print(tr_set2.shape)
 
+# generate test data (true and false)
 te_set1, te_set2, te_y = create_data_siamese_from_images(test_cats_dir, test_dogs_dir, int(data_size / 10))
 
 print(te_set1.shape)
 print(te_set2.shape)
-
-
-# In[6]:
-
-
-'''
-index = 0
-for img in tr_set1:
-    plt.figure(index)
-    imgplot = plt.imshow(img)
-    index += 1
-'''
-
-
-# In[7]:
-
-
-'''
-index = 0
-for img in tr_set2:
-    plt.figure(index)
-    imgplot = plt.imshow(img)
-    index += 1
-'''
-
-
-# In[8]:
-
-
-'''
-index = 0
-for img in te_set1:
-    plt.figure(index)
-    imgplot = plt.imshow(img)
-    index += 1
-'''
-
-
-# In[9]:
-
-
-'''
-index = 0
-for img in te_set2:
-    plt.figure(index)
-    imgplot = plt.imshow(img)
-    index += 1
-'''
-
-
-# In[10]:
 
 
 # network definition
@@ -365,27 +208,20 @@ for img in te_set2:
 input_a = Input(shape=(WIDTH, HEIGHT, 3))
 input_b = Input(shape=(WIDTH, HEIGHT, 3))
 
-# because we re-use the same instance `base_network`,
+# because we re-use the same instance `conv_base`,
 # the weights of the network
 # will be shared across the two branches
-processed_a = conv_base(input_a)#base_network(input_a)
-processed_b = conv_base(input_b)#base_network(input_b)
+processed_a = conv_base(input_a)
+processed_b = conv_base(input_b)
 
 distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
 model = Model(input=[input_a, input_b], output=distance)
 
-
-# In[11]:
-
-
 # train
 rms = RMSprop()
 model.compile(loss=contrastive_loss, optimizer=rms, metrics=['acc'])
 model.summary()
-
-
-# In[ ]:
 
 
 nb_epochs = 10
@@ -397,10 +233,6 @@ history = model.fit([tr_set1, tr_set2], tr_y,
 
 model.save('siamese_cnn_vgg16_1.h5')
 
-
-# In[ ]:
-
-
 # compute final accuracy on training and test sets
 pred = model.predict([tr_set1, tr_set2])
 tr_acc = compute_accuracy(pred, tr_y)
@@ -411,9 +243,6 @@ print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
 print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
 
 
-# In[ ]:
-
-
 acc = history.history['acc']
 val_acc = history.history['val_acc']
 
@@ -421,8 +250,6 @@ loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
-#epochs = range(1, len(loss) + 1)
-
 
 plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
